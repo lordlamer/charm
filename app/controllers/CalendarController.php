591fb202fdf232613a05bb4d98509d184f19d389
lordlamer <lordlamer@lordlamer.de>
2013-03-01 23:46:26 +0100

    * added first stuff for remote calendar access

--- app/controllers/CalendarController.php
+++ app/controllers/CalendarController.php
@@ -14,5 +14,31 @@ class CalendarController extends Zend_Controller_Action {
 	$this->_forward('index');
     }
 
+    public function datesAction() {
+	$this->_helper->layout()->disableLayout();
+        $this->_helper->viewRenderer->setNoRender(true);
+
+$response = $this->getResponse();
+$response->setHeader('Content-type', 'application/json', true);
+
+$date = date("j", time());
+$month = date("n", time());
+$year = date("Y", time());
+$dates = array();
+$dates[] = array(
+    'id' => 123,
+    'title' => 'Click for Google',
+    'start' => '2013-03-28T13:15:30Z',
+    'end' => '2013-03-29T13:15:30Z',
+    //'url' => 'http://google.com/',
+    //'allDay' => false
+    //color
+    //backgroundColor
+    //borderColor
+    //textColor
+);
+
+echo json_encode($dates);
+    }
 }
 
